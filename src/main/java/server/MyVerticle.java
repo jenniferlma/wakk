b36987654bccfbf1422d2413aa3fb6dbd8273895
wendyjan <wendyjan@buffalo.edu>
2015-10-15 19:50:44 -0400

    Basic http response in html form for a User page, Group page, and content page.

--- src/main/java/server/MyVerticle.java
+++ src/main/java/server/MyVerticle.java
@@ -5,6 +5,7 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
 
 public class MyVerticle extends AbstractVerticle {
 
@@ -20,11 +21,44 @@ public class MyVerticle extends AbstractVerticle {
             @Override
             public void handle(HttpServerRequest request) {
                 System.out.println("incoming request!");
-            }
-        });
+                String testPage = request.path(); // could also use request.uri();
+                System.out.println(testPage);
+                HttpServerResponse response = request.response();
+                switch (testPage) {
+                    case "/testUser":
+                        response.setStatusCode(200);
+                        response.headers()
+                                .add("Content-Length", String.valueOf(57))
+                                .add("Content-Type", "text/html");
+                        response.write("<h1>User</h1>");
+                        response.end();
+                        break;
+                    case "/testGroup":
+                        response.setStatusCode(200);
+                        response.headers()
+                                .add("Content-Length", String.valueOf(57))
+                                .add("Content-Type", "text/html");
+                        response.write("<h1>Group</h1>");
+                        response.end();
+                        break;
+                    case "/testContent":
+                        response.setStatusCode(200);
+                        response.headers()
+                                .add("Content-Length", String.valueOf(57))
+                                .add("Content-Type", "text/html");
+                        response.write("<h1>Content</h1>");
+                        response.end();
+                        break;
+                    //TODO Make this a log, add more info
+                    default:
+                        System.out.println("Unsupported Get");
+                }
 
-        _httpServer.listen(9999);
-    }
+            } // end inline handle method
+        }); // end requestHandler call on _httpServer
+
+        _httpServer.listen(9999, "localhost");
+    }// end start method
 
 
     @Override
