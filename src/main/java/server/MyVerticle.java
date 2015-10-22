6fdc43a746d67a6ae0bdbf996d57a44561c9c8aa
kendracl <kendracl@buffalo.edu>
2015-10-22 18:45:20 -0400

    Kendra - Not ideal way to setup Handler. Utilized for Project Submission 1.

--- src/main/java/server/MyVerticle.java
+++ src/main/java/server/MyVerticle.java
@@ -1,5 +1,6 @@
 package server;
 
+import code.User;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
@@ -30,7 +31,20 @@ public class MyVerticle extends AbstractVerticle {
                         response.headers()
                                 .add("Content-Length", String.valueOf(57))
                                 .add("Content-Type", "text/html");
-                        response.write("<h1>User</h1>");
+                        Long internalId = 000000000000000000L;
+                        try {
+                            User user = new User(internalId);
+                            //String user_des = user.toString();
+                            String user_des = "username: ";
+                            user_des += user.get_userName();
+                            user_des += "<br>";
+                            user_des += "id: ";
+                            user_des += user.get_externalId();
+                            user_des = "<h1>" + user_des + "</h1>";
+                            response.write(user_des);
+                            System.out.println("HI");
+                        }
+                        catch (Exception e) {System.out.println(e.getStackTrace());}
                         response.end();
                         break;
                     case "/testGroup":
